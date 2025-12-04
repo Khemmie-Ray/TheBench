574065579a506c3055dc76e41202258f983d8a16
Khemmie-Ray <atokemmy@gmail.com>
2025-12-04 18:27:21 +0100

    edit to test file

--- smart-contract/test/BenchDao.t.sol
+++ smart-contract/test/BenchDao.t.sol
@@ -7,18 +7,5 @@ import {BenchDao} from "../src/BenchDao.sol";
 contract CounterTest is Test {
     BenchDao public benchdao;
 
-    function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
-    }
-
-    function test_Increment() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
-    }
-
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
-    }
+   
 }

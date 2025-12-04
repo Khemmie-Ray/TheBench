ee73e2dd04eb65ae352936999ddf51a81b3165a7
Khemmie-Ray <atokemmy@gmail.com>
2025-12-04 18:25:58 +0100

    minor fixes

--- smart-contract/test/BenchDao.t.sol
+++ smart-contract/test/BenchDao.t.sol
@@ -2,10 +2,10 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import {Counter} from "../src/Counter.sol";
+import {BenchDao} from "../src/BenchDao.sol";
 
 contract CounterTest is Test {
-    Counter public counter;
+    BenchDao public benchdao;
 
     function setUp() public {
         counter = new Counter();
